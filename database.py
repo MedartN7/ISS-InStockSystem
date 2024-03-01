ae68c00396a6d14ab491b890ea2de46bd6c4a0d1
Bartlomiej Walecki <medart87@gmail.com>
2024-03-01 19:50:38 +0100

    01.03.2024

---
+++ database.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+hostname = 'localhost'
+database = 'ISS'
+username = 'postgres'
+pwd = 'DB_ISS'
+#setx DB_PASSWORD twoje_haslo -> do zmiennej środowiskowej zapisujemy hasło do DB Postgres
+port_id = '5050'
