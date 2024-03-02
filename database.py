298d80c8546f2b40dae2c9bd234767ee95a52a93
Bartlomiej Walecki <medart87@gmail.com>
2024-03-02 11:46:53 +0100

    02.03.2024

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
 import psycopg2
+import json
 
-hostname = 'localhost'
-database = 'ISS'
-username = 'postgres'
-pwd = 'DB_ISS'
-#setx DB_PASSWORD twoje_haslo -> do zmiennej środowiskowej zapisujemy hasło do DB Postgres
-port_id = '5050'
+with open('db_config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+
+database_config = config_data.get('database_config', {})
+
+hostname = database_config.get('hostname')
+database = database_config.get('database')
+username = database_config.get('username')
+pwd = database_config.get('password')
+port_id = database_config.get('port')
+
+conn = psycopg2.connect(
+    host=hostname,
+    dbname=database,
+    user=username,
+    password=pwd,
+    port=port_id
+    )
+
